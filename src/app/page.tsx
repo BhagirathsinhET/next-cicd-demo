45bf95d6f3cd25f9efa42b5302cd774451ef7b24
BhagirathsinhET <bhagirathn.encircle@gmail.com>
2026-07-11 10:54:38 +0530

    feat: add demo page

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column",
+      }}
+    >
+      <h1>🚀 Next.js CI/CD Demo</h1>
+
+      <p>If you can see this page, deployment is successful.</p>
+
+      <p>Version: 1.0.0</p>
+    </main>
+  );
+}
